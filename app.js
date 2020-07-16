5191c980f20d804c6fc7f51ba3483910a5487847
Priya Darshini <msvdpriya@gmail.com>
2020-07-16 12:25:47 -0400

    added event listeners

--- app.js
+++ app.js
@@ -1,64 +1,93 @@
 
-function startApp() {
-    var prefix_url = 'https://www.linkedin.com/voyager/api/jobs/jobPostings/';
-    var isAppActive = '';
-    var resumePuppyButton = document.createElement('button'); 
-    resumePuppyButton.innerHTML = 'ResumePuppy';
+function startApp(request) {
+        
+        companyName = document.querySelector(".entry-jobs-top-card__job-title")?.textContent
+        response = {h1b_prob: 'high', gc_prob: 'high'}
+        showButton(response)
+        // var serverURL = server + '/?company=' + companyName;
+        // var xhttp = new XMLHttpRequest();
+        // xhttp.onreadystatechange = function () {
+        //     if (this.readyState == 4 && this.status == 200) {
+        //         // Typical action to be performed when the document is ready:
+        //         showButton(xhttp.response)
+        //     }
+        // };
+        // xhttp.open("GET", serverURL, true);
+        // xhttp.send();
+
+    // var prefix_url = 'https://www.linkedin.com/voyager/api/jobs/jobPostings/';
+
+    // var resumePuppyButton = document.createElement('button'); 
+    // resumePuppyButton.innerHTML = 'ResumePuppy';
     
-    resumePuppyButton.classList.add("artdeco-button"); 
-    resumePuppyButton.classList.add("artdeco-button--3"); 
-    resumePuppyButton.classList.add("ml2"); 
+    // resumePuppyButton.classList.add("artdeco-button"); 
+    // resumePuppyButton.classList.add("artdeco-button--3"); 
+    // resumePuppyButton.classList.add("ml2"); 
      
-    resumePuppyButton.setAttribute("id","resume-puppy-button");
+    // resumePuppyButton.setAttribute("id","resume-puppy-button");
 
-       chrome.storage.sync.get('isAppActive', function (data) {
-        isAppActive = data.isAppActive;
-        // Check if App is Active
-        if (isAppActive) {
-            // console.log('Resume Puppy app active');
-            var currentLink = document.location;
-            let jobId = (new URL(currentLink)).pathname.split("/")[3];
-            var saveUrl = prefix_url+jobId
-            var applyUrl =saveUrl+"?action=applyClick"
-            if (currentLink.href.includes("https://www.linkedin.com/jobs/view/") === true) {
-                document.getElementsByClassName("jobs-top-card__actions")[0].appendChild(resumePuppyButton)
-                document.getElementsByClassName("jobs-apply-button")[0].addEventListener("click", isApplyClicked);
-                console.log("Save URL: "+saveUrl)
-                console.log("Apply URL: "+applyUrl)
-                console.log("Job Title: "+document.querySelector(".entry-jobs-top-card__job-title")?.textContent);
-                console.log("Company Name: "+document.querySelector(".jobs-top-card__company-url")?.textContent);
-                console.log(document.querySelector(".jobs-description-content__text")?.textContent);
+    
+            // // console.log('Resume Puppy app active');
+            // var currentLink = document.location;
+            // let jobId = (new URL(currentLink)).pathname.split("/")[3];
+            // var saveUrl = prefix_url+jobId
+            // var applyUrl =saveUrl+"?action=applyClick"
+            // if (currentLink.href.includes("https://www.linkedin.com/jobs/view/") === true) 
+            // {
+            //     document.getElementsByClassName("jobs-top-card__actions")[0].appendChild(resumePuppyButton)
+            //     document.getElementsByClassName("jobs-apply-button")[0].addEventListener("click", isApplyClicked);
+            //     console.log("Save URL: "+saveUrl)
+            //     console.log("Apply URL: "+applyUrl)
+            //     console.log("Job Title: "+document.querySelector(".entry-jobs-top-card__job-title")?.textContent);
+            //     console.log("Company Name: "+document.querySelector(".jobs-top-card__company-url")?.textContent);
+            //     console.log(document.querySelector(".jobs-description-content__text")?.textContent);
 
-            }
-        }
-    });
+    
+            // }
 }
 
-function isApplyClicked(){
-    console.log("Apply Clicked");
+function showButton(response){
+
+    console.log(response)
+    // var resumePuppyButton = document.createElement('button'); 
+    // resumePuppyButton.innerHTML = 'ResumePuppy';
+    
+    // resumePuppyButton.classList.add("artdeco-button"); 
+    // resumePuppyButton.classList.add("artdeco-button--3"); 
+    // resumePuppyButton.classList.add("ml2"); 
+     
+    // resumePuppyButton.setAttribute("id","resume-puppy-button");
+
 }
-function exitApp(){
-    // console.log('app exiting');
-    var resumePuppyButton = document.querySelector('#resume-puppy-button');
-    if(!(resumePuppyButton == null)){
-        resumePuppyButton.remove()
-    }
-}
+
+// function isApplyClicked(){
+//     console.log("Apply Clicked");
+// }
+
+// function exitApp(){
+//     // console.log('app exiting');
+//     var resumePuppyButton = document.querySelector('#resume-puppy-button');
+//     if(!(resumePuppyButton == null)){
+//         resumePuppyButton.remove()
+//     }
+// }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        // console.log(request);
       // listen for messages sent from popup.js
-      if (request.message === 'enabled') {
-            startApp()
+      if (request.h1b || request.gc) {
+          startApp(request)
+      }
+      else{
+          console.log('remove button')
       }
   });
 
-  chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log(request);
-      // listen for messages sent from background.js
-      if (request.message === 'disabled') {
-        exitApp();
-      }
-  });
+//   chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//         console.log(request);
+//       // listen for messages sent from background.js
+//       if (request.message === 'disabled') {
+//         exitApp();
+//       }
+//   });
--- popup.js
+++ popup.js
@@ -1,56 +1,112 @@
-let activateAppButton = document.getElementById('activate');
-chrome.storage.sync.get('isAppActive', function (data) {
-    var element = activateAppButton;
-    var isAppActive = data.isAppActive;
-    if (!isAppActive) {
-      element.innerText = 'Enable ResumePuppy';
-    }
-    else {
-      element.innerText = 'Disable ResumePuppy';
-    }
+
+let h1bToggleButton = document.getElementById('h1b-toggle');
+let gcToggleButton = document.getElementById('gc-toggle');
+console.log(h1bToggleButton)
+console.log(gcToggleButton)
+// check from storage and accordingly check or uncheck the button
+h1bToggleButton.addEventListener('change', (event) => {
+  toggleClick()
+
+  // save it to browser memory
+  chrome.storage.sync.set({'h1b': event.target?.checked }, function() {
+    console.log('h1b',event.target?.checked);
   });
-activateAppButton.onclick = function (event) {
-    chrome.storage.sync.get('isAppActive', function (data) {
-      var element = event.target;
-    //   console.log(data.isAppActive)
-      var isAppActive = data.isAppActive;
-      if (!isAppActive) {
-        element.innerText = 'Disable ResumePuppy';
-        chrome.storage.sync.set({ isAppActive: true }, function () {
-        // console.log('Resume Puppy App activated');
-        });
-  
-        chrome.tabs.query({'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs){
-            for(var i=0; i< tabs.length; i++){
-              var tab = tabs[i];
-              if(tab.url){
-                if(tab.url.includes("https://www.linkedin.com/jobs/view/") === true){
-                  chrome.tabs.sendMessage( tab.id, {
-                    message: 'enabled'
-                  })
-                }
-              }
-            }   
-          });
-      }
-      else {
-        element.innerText = 'Enable ResumePuppy';
-        chrome.storage.sync.set({ isAppActive: false }, function () {
-        // console.log('Resume Puppy App disabled');
-        });
-  
-        chrome.tabs.query({'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs){
-            for(var i=0; i< tabs.length; i++){
-              var tab = tabs[i];
-              if(tab.url){
-                if(tab.url.includes("https://www.linkedin.com/jobs/view/") === true){
-                  chrome.tabs.sendMessage( tab.id, {
-                    message: 'disabled'
-                  })
-                }
-              }
-            }   
-          });
-      }
-    });
+
+})
+
+gcToggleButton.addEventListener('change', (event) => {
+  toggleClick()
+  chrome.storage.sync.set({'gc': event.target?.checked }, function() {
+    console.log('gc',event.target?.checked);
+  });
+  // save it to browser memory
+})
+
+function toggleClick(){
+  console.log('enabled')
+  if (h1bToggleButton?.checked || gcToggleButton?.checked)
+  {
+    
+    chrome.tabs.query({'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs){
+                  for(var i=0; i< tabs.length; i++){
+                    var tab = tabs[i];
+                    if(tab.url){
+                      if(tab.url.includes("https://www.linkedin.com/jobs/view/") === true){
+                        chrome.tabs.sendMessage( tab.id, 
+                          {
+                          h1b: h1bToggleButton.checked,
+                          gc: gcToggleButton.checked
+                          }
+                      )
+                      }
+                    }
+                  }   
+                });
+  }
+
+
 }
+
+
+
+
+
+
+
+// let activateAppButton = document.getElementById('activate');
+// chrome.storage.sync.get('isAppActive', function (data) {
+//     var element = activateAppButton;
+//     var isAppActive = data.isAppActive;
+//     if (!isAppActive) {
+//       element.innerText = 'Enable ResumePuppy';
+//     }
+//     else {
+//       element.innerText = 'Disable ResumePuppy';
+//     }
+//   });
+
+// activateAppButton.onclick = function (event) {
+//     chrome.storage.sync.get('isAppActive', function (data) {
+//       var element = event.target;
+//     //   console.log(data.isAppActive)
+//       var isAppActive = data.isAppActive;
+//       if (!isAppActive) {
+//         element.innerText = 'Disable ResumePuppy';
+//         chrome.storage.sync.set({ isAppActive: true }, function () {
+//         // console.log('Resume Puppy App activated');
+//         });
+  
+//         chrome.tabs.query({'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs){
+//             for(var i=0; i< tabs.length; i++){
+//               var tab = tabs[i];
+//               if(tab.url){
+//                 if(tab.url.includes("https://www.linkedin.com/jobs/view/") === true){
+//                   chrome.tabs.sendMessage( tab.id, {
+//                     message: 'enabled'
+//                   })
+//                 }
+//               }
+//             }   
+//           });
+//       }
+//       else {
+//         element.innerText = 'Enable ResumePuppy';
+//         chrome.storage.sync.set({ isAppActive: false }, function () {
+//         // console.log('Resume Puppy App disabled');
+//         });
+  
+//         chrome.tabs.query({'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs){
+//             for(var i=0; i< tabs.length; i++){
+//               var tab = tabs[i];
+//               if(tab.url){
+//                 if(tab.url.includes("https://www.linkedin.com/jobs/view/") === true){
+//                   chrome.tabs.sendMessage( tab.id, {
+//                     message: 'disabled'
+//                   })
+//                 }
+//               }
+//             }   
+//           });
+//       }
+//     });
+// }
