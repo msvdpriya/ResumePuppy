dd839efc64e8b3b2aff9a4ad9571103884325f6b
Priya Darshini <msvdpriya@gmail.com>
2020-07-17 12:12:51 -0400

    updates

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    // chrome.storage.sync.set({'h1b': false}, function() {
-    //     console.log("The h1b is stored at bg.js.");
-    //   });
-    //   chrome.storage.sync.set({'gc': false}, function() {
-    //     console.log("The gc is stored at bg.js.");
-    //   });
+
     console.log("ResumePuppy is installed");
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
@@ -17,4 +12,5 @@ chrome.runtime.onInstalled.addListener(function() {
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
+
 })
--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@
   // the DOM will be available here
   let h1bToggleButton = document.getElementById("h1b-toggle");
   let gcToggleButton = document.getElementById("gc-toggle");
+  let linkedUrl = "https://www.linkedin.com/jobs/view/"
 
   // check from storage and accordingly check or uncheck the button
   chrome.storage.sync.get("h1b", function (data) {
@@ -15,7 +16,7 @@
   });
 
   
-
+  // add event listener for the change of toggle button
   h1bToggleButton.addEventListener("change", (event) => {
     toggleClick();
     // save it to browser memory
@@ -27,94 +28,24 @@
     // save it to browser memory
     chrome.storage.sync.set({ gc: event.target?.checked }, function () {});
   });
-
+  //function that sendMessage for app indicating the change that happened
   function toggleClick() {
-    console.log("enabled");
-    console.log(h1bToggleButton)
-    console.log(gcToggleButton)
-    console.log(h1bToggleButton.checked)
-    console.log(gcToggleButton.checked)
-
-
-    if (h1bToggleButton?.checked || gcToggleButton?.checked) {
       chrome.tabs.query(
         { windowId: chrome.windows.WINDOW_ID_CURRENT },
         function (tabs) {
           for (var i = 0; i < tabs.length; i++) {
             var tab = tabs[i];
-            if (tab.url) {
-              if (
-                tab.url.includes("https://www.linkedin.com/jobs/view/") === true
-              ) {
+            if (tab.url?.includes(linkedUrl) === true) {
                 chrome.tabs.sendMessage(tab.id, {
-                  h1b: h1bToggleButton.checked,
-                  gc: gcToggleButton.checked,
+                  h1b: h1bToggleButton?.checked,
+                  gc: gcToggleButton?.checked,
                 });
-              }
+              
             }
           }
         }
       );
-    }
+    
   }
-
-  // let activateAppButton = document.getElementById('activate');
-  // chrome.storage.sync.get('isAppActive', function (data) {
-  //     var element = activateAppButton;
-  //     var isAppActive = data.isAppActive;
-  //     if (!isAppActive) {
-  //       element.innerText = 'Enable ResumePuppy';
-  //     }
-  //     else {
-  //       element.innerText = 'Disable ResumePuppy';
-  //     }
-  //   });
-
-  // activateAppButton.onclick = function (event) {
-  //     chrome.storage.sync.get('isAppActive', function (data) {
-  //       var element = event.target;
-  //     //   console.log(data.isAppActive)
-  //       var isAppActive = data.isAppActive;
-  //       if (!isAppActive) {
-  //         element.innerText = 'Disable ResumePuppy';
-  //         chrome.storage.sync.set({ isAppActive: true }, function () {
-  //         // console.log('Resume Puppy App activated');
-  //         });
-
-  //         chrome.tabs.query({'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs){
-  //             for(var i=0; i< tabs.length; i++){
-  //               var tab = tabs[i];
-  //               if(tab.url){
-  //                 if(tab.url.includes("https://www.linkedin.com/jobs/view/") === true){
-  //                   chrome.tabs.sendMessage( tab.id, {
-  //                     message: 'enabled'
-  //                   })
-  //                 }
-  //               }
-  //             }
-  //           });
-  //       }
-  //       else {
-  //         element.innerText = 'Enable ResumePuppy';
-  //         chrome.storage.sync.set({ isAppActive: false }, function () {
-  //         // console.log('Resume Puppy App disabled');
-  //         });
-
-  //         chrome.tabs.query({'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs){
-  //             for(var i=0; i< tabs.length; i++){
-  //               var tab = tabs[i];
-  //               if(tab.url){
-  //                 if(tab.url.includes("https://www.linkedin.com/jobs/view/") === true){
-  //                   chrome.tabs.sendMessage( tab.id, {
-  //                     message: 'disabled'
-  //                   })
-  //                 }
-  //               }
-  //             }
-  //           });
-  //       }
-  //     });
-  // }
-
 })();
   
